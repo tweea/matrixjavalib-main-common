95f1e0528af0ca0d977e6c26f329129b21f9cca0
Tweea <tweea@263.net>
2014-12-17 09:51:45 +0800

    增加 BigDecimals

---
+++ src/test/java/net/matrix/util/BigDecimalsTest.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright(C) 2008 Matrix
+ * All right reserved.
+ */
+package net.matrix.util;
+
+import java.math.BigDecimal;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class BigDecimalsTest {
+	@Test
+	public void equals() {
+		Assertions.assertThat(BigDecimals.equals(new BigDecimal("1"), new BigDecimal("1.0"))).isTrue();
+		Assertions.assertThat(BigDecimals.equals(new BigDecimal("1"), new BigDecimal("2.0"))).isFalse();
+	}
+}
