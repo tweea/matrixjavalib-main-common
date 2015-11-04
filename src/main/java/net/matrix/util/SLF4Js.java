a3b402797bb112b2ee434bed1428dd9b388c0ca3
Tweea <tweea@263.net>
2015-11-04 10:13:58 +0800

    SLF4J 工具类

---
+++ src/main/java/net/matrix/util/SLF4Js.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright(C) 2011 Matrix
+ * All right reserved.
+ */
+package net.matrix.util;
+
+import org.slf4j.bridge.SLF4JBridgeHandler;
+
+/**
+ * SLF4J 工具类。
+ */
+public final class SLF4Js {
+	/**
+	 * 阻止实例化。
+	 */
+	private SLF4Js() {
+	}
+
+	/**
+	 * 将 java.util.logging 的日志代理到 SLF4J。
+	 */
+	public static void bridgeJUL() {
+		if (!SLF4JBridgeHandler.isInstalled()) {
+			SLF4JBridgeHandler.removeHandlersForRootLogger();
+			SLF4JBridgeHandler.install();
+		}
+	}
+}
