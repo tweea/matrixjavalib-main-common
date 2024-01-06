63520f6866f61fd5d84f34cb762c4896d3869e98
Tweea <tweea@qq.com>
2024-01-06 21:47:26 +0800

    日期时间工具

---
+++ src/main/java/net/matrix/java/time/DateTimeMx.java
@@ -0,0 +1,249 @@
+/*
+ * 版权所有 2024 Matrix。
+ * 保留所有权利。
+ */
+package net.matrix.java.time;
+
+import java.time.DateTimeException;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 日期时间工具。
+ */
+public final class DateTimeMx {
+    /**
+     * 日志记录器。
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(DateTimeMx.class);
+
+    /**
+     * 阻止实例化。
+     */
+    private DateTimeMx() {
+    }
+
+    /**
+     * 校验日期的年、月、日数值是否符合历法。
+     * 
+     * @param year
+     *     年。
+     * @param month
+     *     月。
+     * @param day
+     *     日。
+     * @return 是否符合历法。
+     */
+    public static boolean isValidDate(int year, int month, int day) {
+        try {
+            LocalDate.of(year, month, day);
+            return true;
+        } catch (DateTimeException e) {
+            LOG.trace("", e);
+            return false;
+        }
+    }
+
+    /**
+     * 校验时间的时、分、秒数值是否符合历法。
+     * 
+     * @param hour
+     *     时。
+     * @param minute
+     *     分。
+     * @param second
+     *     秒。
+     * @return 是否符合历法。
+     */
+    public static boolean isValidTime(int hour, int minute, int second) {
+        try {
+            LocalTime.of(hour, minute, second);
+            return true;
+        } catch (DateTimeException e) {
+            LOG.trace("", e);
+            return false;
+        }
+    }
+
+    /**
+     * 将参数转换为 {@link Instant} 对象。
+     * 
+     * @param object
+     *     表示日期时间的对象。
+     * @return {@link Instant} 对象。
+     */
+    public static Instant toInstant(Object object) {
+        if (object == null) {
+            return null;
+        }
+
+        if (object instanceof Long) {
+            return Instant.ofEpochMilli((long) object);
+        }
+        if (object instanceof Date) {
+            return ((Date) object).toInstant();
+        }
+        if (object instanceof Calendar) {
+            return ((Calendar) object).toInstant();
+        }
+
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * 将参数转换为 {@link LocalDate} 对象。
+     * 
+     * @param object
+     *     表示日期时间的对象。
+     * @param zoneId
+     *     时区 ID，为 <code>null</code> 时使用系统默认时区 ID。
+     * @return {@link LocalDate} 对象。
+     */
+    public static LocalDate toLocalDate(Object object, ZoneId zoneId) {
+        Instant instant = toInstant(object);
+        if (instant == null) {
+            return null;
+        }
+
+        if (zoneId == null) {
+            zoneId = ZoneId.systemDefault();
+        }
+
+        return instant.atZone(zoneId).toLocalDate();
+    }
+
+    /**
+     * 将参数转换为 {@link LocalTime} 对象。
+     * 
+     * @param object
+     *     表示日期时间的对象。
+     * @param zoneId
+     *     时区 ID，为 <code>null</code> 时使用系统默认时区 ID。
+     * @return {@link LocalTime} 对象。
+     */
+    public static LocalTime toLocalTime(Object object, ZoneId zoneId) {
+        Instant instant = toInstant(object);
+        if (instant == null) {
+            return null;
+        }
+
+        if (zoneId == null) {
+            zoneId = ZoneId.systemDefault();
+        }
+
+        return instant.atZone(zoneId).toLocalTime();
+    }
+
+    /**
+     * 将参数转换为 {@link LocalDateTime} 对象。
+     * 
+     * @param object
+     *     表示日期时间的对象。
+     * @param zoneId
+     *     时区 ID，为 <code>null</code> 时使用系统默认时区 ID。
+     * @return {@link LocalDateTime} 对象。
+     */
+    public static LocalDateTime toLocalDateTime(Object object, ZoneId zoneId) {
+        Instant instant = toInstant(object);
+        if (instant == null) {
+            return null;
+        }
+
+        if (zoneId == null) {
+            zoneId = ZoneId.systemDefault();
+        }
+
+        return instant.atZone(zoneId).toLocalDateTime();
+    }
+
+    private static Instant toInstant(Object object, ZoneId zoneId) {
+        if (object instanceof Instant) {
+            return (Instant) object;
+        }
+        if (object instanceof LocalDate) {
+            return ((LocalDate) object).atStartOfDay(zoneId).toInstant();
+        }
+        if (object instanceof LocalTime) {
+            return ((LocalTime) object).atDate(LocalDate.now()).atZone(zoneId).toInstant();
+        }
+        if (object instanceof LocalDateTime) {
+            return ((LocalDateTime) object).atZone(zoneId).toInstant();
+        }
+
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * 将参数转换为自 1970-01-01T00:00:00Z 以来的毫秒数。
+     * 
+     * @param object
+     *     表示日期时间的对象。
+     * @param zoneId
+     *     时区 ID，为 <code>null</code> 时使用系统默认时区 ID。
+     * @return 毫秒数。
+     */
+    public static Long toEpochMilli(Object object, ZoneId zoneId) {
+        if (object == null) {
+            return null;
+        }
+
+        if (zoneId == null) {
+            zoneId = ZoneId.systemDefault();
+        }
+
+        return toInstant(object, zoneId).toEpochMilli();
+    }
+
+    /**
+     * 将参数转换为 {@link Date} 对象。
+     * 
+     * @param object
+     *     表示日期时间的对象。
+     * @param zoneId
+     *     时区 ID，为 <code>null</code> 时使用系统默认时区 ID。
+     * @return {@link Date} 对象。
+     */
+    public static Date toDate(Object object, ZoneId zoneId) {
+        if (object == null) {
+            return null;
+        }
+
+        if (zoneId == null) {
+            zoneId = ZoneId.systemDefault();
+        }
+
+        return Date.from(toInstant(object, zoneId));
+    }
+
+    /**
+     * 将参数转换为 {@link Calendar} 对象。
+     * 
+     * @param object
+     *     表示日期时间的对象。
+     * @param zoneId
+     *     时区 ID，为 <code>null</code> 时使用系统默认时区 ID。
+     * @return {@link Calendar} 对象。
+     */
+    public static Calendar toCalendar(Object object, ZoneId zoneId) {
+        if (object == null) {
+            return null;
+        }
+
+        if (zoneId == null) {
+            zoneId = ZoneId.systemDefault();
+        }
+
+        Date date = Date.from(toInstant(object, zoneId));
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar;
+    }
+}
---
+++ src/test/java/net/matrix/java/time/DateTimeMxTest.java
@@ -0,0 +1,94 @@
+/*
+ * 版权所有 2024 Matrix。
+ * 保留所有权利。
+ */
+package net.matrix.java.time;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DateTimeMxTest {
+    @Test
+    public void testIsValidDate() {
+        assertThat(DateTimeMx.isValidDate(1948, 5, 1)).isTrue();
+        assertThat(DateTimeMx.isValidDate(2011, 10, 1)).isTrue();
+        assertThat(DateTimeMx.isValidDate(2011, 13, 1)).isFalse();
+        assertThat(DateTimeMx.isValidDate(2011, 2, 31)).isFalse();
+    }
+
+    @Test
+    public void testIsValidTime() {
+        assertThat(DateTimeMx.isValidTime(2, 2, 2)).isTrue();
+        assertThat(DateTimeMx.isValidTime(18, 18, 18)).isTrue();
+        assertThat(DateTimeMx.isValidTime(25, 18, 18)).isFalse();
+        assertThat(DateTimeMx.isValidTime(18, 68, 18)).isFalse();
+        assertThat(DateTimeMx.isValidTime(18, 18, 68)).isFalse();
+    }
+
+    @Test
+    public void testToInstant() {
+        assertThat(DateTimeMx.toInstant(null)).isNull();
+        assertThat(DateTimeMx.toInstant(1L)).isNotNull();
+        assertThat(DateTimeMx.toInstant(new Date())).isNotNull();
+        assertThat(DateTimeMx.toInstant(Calendar.getInstance())).isNotNull();
+    }
+
+    @Test
+    public void testToLocalDate() {
+        assertThat(DateTimeMx.toLocalDate(null, null)).isNull();
+        assertThat(DateTimeMx.toLocalDate(1L, null)).isNotNull();
+        assertThat(DateTimeMx.toLocalDate(new Date(), null)).isNotNull();
+        assertThat(DateTimeMx.toLocalDate(Calendar.getInstance(), null)).isNotNull();
+    }
+
+    @Test
+    public void testToLocalTime() {
+        assertThat(DateTimeMx.toLocalTime(null, null)).isNull();
+        assertThat(DateTimeMx.toLocalTime(1L, null)).isNotNull();
+        assertThat(DateTimeMx.toLocalTime(new Date(), null)).isNotNull();
+        assertThat(DateTimeMx.toLocalTime(Calendar.getInstance(), null)).isNotNull();
+    }
+
+    @Test
+    public void testToLocalDateTime() {
+        assertThat(DateTimeMx.toLocalDateTime(null, null)).isNull();
+        assertThat(DateTimeMx.toLocalDateTime(1L, null)).isNotNull();
+        assertThat(DateTimeMx.toLocalDateTime(new Date(), null)).isNotNull();
+        assertThat(DateTimeMx.toLocalDateTime(Calendar.getInstance(), null)).isNotNull();
+    }
+
+    @Test
+    public void testToEpochMilli() {
+        assertThat(DateTimeMx.toEpochMilli(null, null)).isNull();
+        assertThat(DateTimeMx.toEpochMilli(Instant.now(), null)).isNotNull();
+        assertThat(DateTimeMx.toEpochMilli(LocalDate.now(), null)).isNotNull();
+        assertThat(DateTimeMx.toEpochMilli(LocalTime.now(), null)).isNotNull();
+        assertThat(DateTimeMx.toEpochMilli(LocalDateTime.now(), null)).isNotNull();
+    }
+
+    @Test
+    public void testToDate() {
+        assertThat(DateTimeMx.toDate(null, null)).isNull();
+        assertThat(DateTimeMx.toDate(Instant.now(), null)).isNotNull();
+        assertThat(DateTimeMx.toDate(LocalDate.now(), null)).isNotNull();
+        assertThat(DateTimeMx.toDate(LocalTime.now(), null)).isNotNull();
+        assertThat(DateTimeMx.toDate(LocalDateTime.now(), null)).isNotNull();
+    }
+
+    @Test
+    public void testToCalendar() {
+        assertThat(DateTimeMx.toCalendar(null, null)).isNull();
+        assertThat(DateTimeMx.toCalendar(Instant.now(), null)).isNotNull();
+        assertThat(DateTimeMx.toCalendar(LocalDate.now(), null)).isNotNull();
+        assertThat(DateTimeMx.toCalendar(LocalTime.now(), null)).isNotNull();
+        assertThat(DateTimeMx.toCalendar(LocalDateTime.now(), null)).isNotNull();
+    }
+}
