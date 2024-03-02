42704b45e189214c48e7665286c44a8e1ab0c917
Tweea <tweea@qq.com>
2024-03-02 20:21:50 +0800

    UncheckedException

---
+++ src/main/java/net/matrix/java/lang/UncheckedException.java
@@ -0,0 +1,48 @@
+/*
+ * 版权所有 2024 Matrix。
+ * 保留所有权利。
+ */
+package net.matrix.java.lang;
+
+import java.util.Objects;
+
+/**
+ * 包装检查异常的非检查异常。
+ */
+public class UncheckedException
+    extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 使用指定原因异常构造异常，详细信息指定为 <code>cause.toString()</code> （特别地指定原因异常的类和详细信息）。
+     * 
+     * @param cause
+     *     原因异常（使用 {@link #getCause()} 方法获取）。
+     */
+    public UncheckedException(Throwable cause) {
+        super(cause.toString());
+        initCause(getInitCause(cause));
+    }
+
+    /**
+     * 使用指定详细信息和原因异常构造异常。<br>
+     * 注意与 <code>cause</code> 关联的详细信息<i>不会</i>自动出现在本异常的详细信息中。
+     * 
+     * @param message
+     *     详细信息。详细信息可以通过 {@link #getMessage()} 方法获取。
+     * @param cause
+     *     原因异常（使用 {@link #getCause()} 方法获取）。
+     */
+    public UncheckedException(String message, Throwable cause) {
+        super(message);
+        Objects.requireNonNull(cause);
+        initCause(getInitCause(cause));
+    }
+
+    /**
+     * 获取原因异常。
+     */
+    protected Throwable getInitCause(Throwable cause) {
+        return cause;
+    }
+}
---
+++ src/test/java/net/matrix/java/lang/UncheckedExceptionTest.java
@@ -0,0 +1,30 @@
+/*
+ * 版权所有 2024 Matrix。
+ * 保留所有权利。
+ */
+package net.matrix.java.lang;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UncheckedExceptionTest {
+    @Test
+    public void testNew_cause() {
+        Exception cause = new Exception();
+
+        UncheckedException exception = new UncheckedException(cause);
+        assertThat(exception.getMessage()).isEqualTo(cause.toString());
+        assertThat(exception.getCause()).isSameAs(cause);
+    }
+
+    @Test
+    public void testNew_message_cause() {
+        String message = "t";
+        Exception cause = new Exception();
+
+        UncheckedException exception = new UncheckedException(message, cause);
+        assertThat(exception.getMessage()).isEqualTo(message);
+        assertThat(exception.getCause()).isSameAs(cause);
+    }
+}
