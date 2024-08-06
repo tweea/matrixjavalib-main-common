6462b786b9cc33c23ab736beb2d0f10dc9c9fca4
Tweea <tweea@qq.com>
2024-08-06 23:49:45 +0800

    NumberMx

---
+++ src/main/java/net/matrix/java/lang/NumberMx.java
@@ -0,0 +1,324 @@
+/*
+ * 版权所有 2024 Matrix。
+ * 保留所有权利。
+ */
+package net.matrix.java.lang;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.annotation.concurrent.ThreadSafe;
+
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+
+/**
+ * 数字工具。
+ */
+@ThreadSafe
+public final class NumberMx {
+    /**
+     * 阻止实例化。
+     */
+    private NumberMx() {
+    }
+
+    /**
+     * 解析字符串为 {@code int} 类型。
+     * 
+     * @param string
+     *     字符串。
+     * @return {@code int} 类型。
+     */
+    public static int toInt(@Nullable String string) {
+        return toInt(string, 0);
+    }
+
+    /**
+     * 解析字符串为 {@code int} 类型。
+     * 
+     * @param string
+     *     字符串。
+     * @param defaultValue
+     *     默认值。
+     * @return {@code int} 类型。
+     */
+    public static int toInt(@Nullable String string, int defaultValue) {
+        if (StringUtils.isBlank(string)) {
+            return defaultValue;
+        }
+
+        return Integer.parseInt(string);
+    }
+
+    /**
+     * 解析字符串为 {@code long} 类型。
+     * 
+     * @param string
+     *     字符串。
+     * @return {@code long} 类型。
+     */
+    public static long toLong(@Nullable String string) {
+        return toLong(string, 0);
+    }
+
+    /**
+     * 解析字符串为 {@code long} 类型。
+     * 
+     * @param string
+     *     字符串。
+     * @param defaultValue
+     *     默认值。
+     * @return {@code long} 类型。
+     */
+    public static long toLong(@Nullable String string, long defaultValue) {
+        if (StringUtils.isBlank(string)) {
+            return defaultValue;
+        }
+
+        return Long.parseLong(string);
+    }
+
+    /**
+     * 解析字符串为 {@code float} 类型。
+     * 
+     * @param string
+     *     字符串。
+     * @return {@code float} 类型。
+     */
+    public static float toFloat(@Nullable String string) {
+        return toFloat(string, 0);
+    }
+
+    /**
+     * 解析字符串为 {@code float} 类型。
+     * 
+     * @param string
+     *     字符串。
+     * @param defaultValue
+     *     默认值。
+     * @return {@code float} 类型。
+     */
+    public static float toFloat(@Nullable String string, float defaultValue) {
+        if (StringUtils.isBlank(string)) {
+            return defaultValue;
+        }
+
+        return Float.parseFloat(string);
+    }
+
+    /**
+     * 解析字符串为 {@code double} 类型。
+     * 
+     * @param string
+     *     字符串。
+     * @return {@code double} 类型。
+     */
+    public static double toDouble(@Nullable String string) {
+        return toDouble(string, 0);
+    }
+
+    /**
+     * 解析字符串为 {@code double} 类型。
+     * 
+     * @param string
+     *     字符串。
+     * @param defaultValue
+     *     默认值。
+     * @return {@code double} 类型。
+     */
+    public static double toDouble(@Nullable String string, double defaultValue) {
+        if (StringUtils.isBlank(string)) {
+            return defaultValue;
+        }
+
+        return Double.parseDouble(string);
+    }
+
+    /**
+     * 解析字符串为 {@link Number} 类型。
+     * 
+     * @param string
+     *     字符串。
+     * @return {@link Number} 类型。
+     */
+    @Nullable
+    public static Number parseNumber(@Nullable String string) {
+        if (StringUtils.isBlank(string)) {
+            return null;
+        }
+
+        return NumberUtils.createNumber(string);
+    }
+
+    /**
+     * 解析字符串为 {@link Integer} 类型。
+     * 
+     * @param string
+     *     字符串。
+     * @return {@link Integer} 类型。
+     */
+    @Nullable
+    public static Integer parseInteger(@Nullable String string) {
+        if (StringUtils.isBlank(string)) {
+            return null;
+        }
+
+        return Integer.decode(string);
+    }
+
+    /**
+     * 解析字符串为 {@link Long} 类型。
+     * 
+     * @param string
+     *     字符串。
+     * @return {@link Long} 类型。
+     */
+    @Nullable
+    public static Long parseLong(@Nullable String string) {
+        if (StringUtils.isBlank(string)) {
+            return null;
+        }
+
+        return Long.decode(string);
+    }
+
+    /**
+     * 解析字符串为 {@link Float} 类型。
+     * 
+     * @param string
+     *     字符串。
+     * @return {@link Float} 类型。
+     */
+    @Nullable
+    public static Float parseFloat(@Nullable String string) {
+        if (StringUtils.isBlank(string)) {
+            return null;
+        }
+
+        return Float.valueOf(string);
+    }
+
+    /**
+     * 解析字符串为 {@link Double} 类型。
+     * 
+     * @param string
+     *     字符串。
+     * @return {@link Double} 类型。
+     */
+    @Nullable
+    public static Double parseDouble(@Nullable String string) {
+        if (StringUtils.isBlank(string)) {
+            return null;
+        }
+
+        return Double.valueOf(string);
+    }
+
+    /**
+     * 解析字符串为 {@link BigInteger} 类型。
+     * 
+     * @param string
+     *     字符串。
+     * @return {@link BigInteger} 类型。
+     */
+    @Nullable
+    public static BigInteger parseBigInteger(@Nullable String string) {
+        if (StringUtils.isBlank(string)) {
+            return null;
+        }
+
+        return NumberUtils.createBigInteger(string);
+    }
+
+    /**
+     * 解析字符串为 {@link BigDecimal} 类型。
+     * 
+     * @param string
+     *     字符串。
+     * @return {@link BigDecimal} 类型。
+     */
+    @Nullable
+    public static BigDecimal parseBigDecimal(@Nullable String string) {
+        if (StringUtils.isBlank(string)) {
+            return null;
+        }
+
+        return new BigDecimal(string);
+    }
+
+    /**
+     * 将 {@link Integer} 类型 <code>null</code> 值转换为 0。
+     * 
+     * @param value
+     *     数值。
+     * @return {@link Integer} 类型。
+     */
+    @Nonnull
+    public static Integer zeroIfNull(@Nullable Integer value) {
+        return ObjectUtils.defaultIfNull(value, NumberUtils.INTEGER_ZERO);
+    }
+
+    /**
+     * 将 {@link Long} 类型 <code>null</code> 值转换为 0。
+     * 
+     * @param value
+     *     数值。
+     * @return {@link Long} 类型。
+     */
+    @Nonnull
+    public static Long zeroIfNull(@Nullable Long value) {
+        return ObjectUtils.defaultIfNull(value, NumberUtils.LONG_ZERO);
+    }
+
+    /**
+     * 将 {@link Float} 类型 <code>null</code> 值转换为 0。
+     * 
+     * @param value
+     *     数值。
+     * @return {@link Float} 类型。
+     */
+    @Nonnull
+    public static Float zeroIfNull(@Nullable Float value) {
+        return ObjectUtils.defaultIfNull(value, NumberUtils.FLOAT_ZERO);
+    }
+
+    /**
+     * 将 {@link Double} 类型 <code>null</code> 值转换为 0。
+     * 
+     * @param value
+     *     数值。
+     * @return {@link Double} 类型。
+     */
+    @Nonnull
+    public static Double zeroIfNull(@Nullable Double value) {
+        return ObjectUtils.defaultIfNull(value, NumberUtils.DOUBLE_ZERO);
+    }
+
+    /**
+     * 将 {@link BigInteger} 类型 <code>null</code> 值转换为 0。
+     * 
+     * @param value
+     *     数值。
+     * @return {@link BigInteger} 类型。
+     */
+    @Nonnull
+    public static BigInteger zeroIfNull(@Nullable BigInteger value) {
+        return ObjectUtils.defaultIfNull(value, BigInteger.ZERO);
+    }
+
+    /**
+     * 将 {@link BigDecimal} 类型 <code>null</code> 值转换为 0。
+     * 
+     * @param value
+     *     数值。
+     * @return {@link BigDecimal} 类型。
+     */
+    @Nonnull
+    public static BigDecimal zeroIfNull(@Nullable BigDecimal value) {
+        return ObjectUtils.defaultIfNull(value, BigDecimal.ZERO);
+    }
+}
---
+++ src/test/java/net/matrix/java/lang/NumberMxTest.java
@@ -0,0 +1,155 @@
+/*
+ * 版权所有 2024 Matrix。
+ * 保留所有权利。
+ */
+package net.matrix.java.lang;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class NumberMxTest {
+    @Test
+    void testToInt() {
+        assertThat(NumberMx.toInt(null)).isEqualTo(0);
+        assertThat(NumberMx.toInt("  \n")).isEqualTo(0);
+        assertThat(NumberMx.toInt("5")).isEqualTo(5);
+    }
+
+    @Test
+    void testToInt_defaultValue() {
+        assertThat(NumberMx.toInt(null, 1)).isEqualTo(1);
+        assertThat(NumberMx.toInt("  \n", 1)).isEqualTo(1);
+        assertThat(NumberMx.toInt("5", 1)).isEqualTo(5);
+    }
+
+    @Test
+    void testToLong() {
+        assertThat(NumberMx.toLong(null)).isEqualTo(0);
+        assertThat(NumberMx.toLong("  \n")).isEqualTo(0);
+        assertThat(NumberMx.toLong("5")).isEqualTo(5);
+    }
+
+    @Test
+    void testToLong_defaultValue() {
+        assertThat(NumberMx.toLong(null, 1)).isEqualTo(1);
+        assertThat(NumberMx.toLong("  \n", 1)).isEqualTo(1);
+        assertThat(NumberMx.toLong("5", 1)).isEqualTo(5);
+    }
+
+    @Test
+    void testToFloat() {
+        assertThat(NumberMx.toFloat(null)).isEqualTo(0);
+        assertThat(NumberMx.toFloat("  \n")).isEqualTo(0);
+        assertThat(NumberMx.toFloat("5")).isEqualTo(5);
+    }
+
+    @Test
+    void testToFloat_defaultValue() {
+        assertThat(NumberMx.toFloat(null, 1)).isEqualTo(1);
+        assertThat(NumberMx.toFloat("  \n", 1)).isEqualTo(1);
+        assertThat(NumberMx.toFloat("5", 1)).isEqualTo(5);
+    }
+
+    @Test
+    void testToDouble() {
+        assertThat(NumberMx.toDouble(null)).isEqualTo(0);
+        assertThat(NumberMx.toDouble("  \n")).isEqualTo(0);
+        assertThat(NumberMx.toDouble("5")).isEqualTo(5);
+    }
+
+    @Test
+    void testToDouble_defaultValue() {
+        assertThat(NumberMx.toDouble(null, 1)).isEqualTo(1);
+        assertThat(NumberMx.toDouble("  \n", 1)).isEqualTo(1);
+        assertThat(NumberMx.toDouble("5", 1)).isEqualTo(5);
+    }
+
+    @Test
+    void testParseNumber() {
+        assertThat(NumberMx.parseNumber(null)).isNull();
+        assertThat(NumberMx.parseNumber("  \n")).isNull();
+        assertThat(NumberMx.parseNumber("5")).isEqualTo(5);
+    }
+
+    @Test
+    void testParseInteger() {
+        assertThat(NumberMx.parseInteger(null)).isNull();
+        assertThat(NumberMx.parseInteger("  \n")).isNull();
+        assertThat(NumberMx.parseInteger("5")).isEqualTo(5);
+    }
+
+    @Test
+    void testParseLong() {
+        assertThat(NumberMx.parseLong(null)).isNull();
+        assertThat(NumberMx.parseLong("  \n")).isNull();
+        assertThat(NumberMx.parseLong("5")).isEqualTo(5);
+    }
+
+    @Test
+    void testParseFloat() {
+        assertThat(NumberMx.parseFloat(null)).isNull();
+        assertThat(NumberMx.parseFloat("  \n")).isNull();
+        assertThat(NumberMx.parseFloat("5")).isEqualTo(5);
+    }
+
+    @Test
+    void testParseDouble() {
+        assertThat(NumberMx.parseDouble(null)).isNull();
+        assertThat(NumberMx.parseDouble("  \n")).isNull();
+        assertThat(NumberMx.parseDouble("5")).isEqualTo(5);
+    }
+
+    @Test
+    void testParseBigInteger() {
+        assertThat(NumberMx.parseBigInteger(null)).isNull();
+        assertThat(NumberMx.parseBigInteger("  \n")).isNull();
+        assertThat(NumberMx.parseBigInteger("5")).isEqualTo("5");
+    }
+
+    @Test
+    void testParseBigDecimal() {
+        assertThat(NumberMx.parseBigDecimal(null)).isNull();
+        assertThat(NumberMx.parseBigDecimal("  \n")).isNull();
+        assertThat(NumberMx.parseBigDecimal("5")).isEqualTo("5");
+    }
+
+    @Test
+    void testZeroIfNull_Integer() {
+        assertThat(NumberMx.zeroIfNull((Integer) null)).isEqualTo(0);
+        assertThat(NumberMx.zeroIfNull(5)).isEqualTo(5);
+    }
+
+    @Test
+    void testZeroIfNull_Long() {
+        assertThat(NumberMx.zeroIfNull((Long) null)).isEqualTo(0);
+        assertThat(NumberMx.zeroIfNull(5L)).isEqualTo(5);
+    }
+
+    @Test
+    void testZeroIfNull_Float() {
+        assertThat(NumberMx.zeroIfNull((Float) null)).isEqualTo(0);
+        assertThat(NumberMx.zeroIfNull(5F)).isEqualTo(5);
+    }
+
+    @Test
+    void testZeroIfNull_Double() {
+        assertThat(NumberMx.zeroIfNull((Double) null)).isEqualTo(0);
+        assertThat(NumberMx.zeroIfNull(5D)).isEqualTo(5);
+    }
+
+    @Test
+    void testZeroIfNull_BigInteger() {
+        assertThat(NumberMx.zeroIfNull((BigInteger) null)).isEqualTo(BigInteger.ZERO);
+        assertThat(NumberMx.zeroIfNull(BigInteger.valueOf(5))).isEqualTo("5");
+    }
+
+    @Test
+    void testZeroIfNull_BigDecimal() {
+        assertThat(NumberMx.zeroIfNull((BigDecimal) null)).isEqualTo(BigDecimal.ZERO);
+        assertThat(NumberMx.zeroIfNull(BigDecimal.valueOf(5))).isEqualTo("5");
+    }
+}
